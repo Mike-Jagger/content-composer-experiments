a5c831bf79ecbc39efcdd75aa1d3adf9a05ba9d3
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-12 15:56:15 -0600

    Adding initial implementation of fetchMovies function

--- src/simpleApp/MovieApp.jsx
+++ src/simpleApp/MovieApp.jsx
@@ -1,7 +1,14 @@
 import "./MovieApp.css";
+import SearchIcon from "./search.svg";
+
+const fetchMovies = async (movieName) => {
+	const response = await fetch({
+		url: "http://www.omdbapi.com/?i=tt3896198&apikey=8744df25",
+	});
+};
 
 const MovieApp = () => {
-	return <div>MovieApp</div>;
+	return <h1>Movie App</h1>;
 };
 
 export default MovieApp;
