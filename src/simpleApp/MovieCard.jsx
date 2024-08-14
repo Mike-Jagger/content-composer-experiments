a3d461d1518ad9f34ff68cd814f73c840e5fce1d
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-14 02:56:16 -0600

    Adding definition of movie card render

--- src/simpleApp/MovieCard.jsx
+++ src/simpleApp/MovieCard.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const MovieCard = () => {
-	return <div>MovieCard</div>;
+const MovieCard = ({ movieData }) => {
+	return (
+		<div className="movie">
+			<div>
+				<p>{movieData.Title}</p>
+			</div>
+
+			<div>
+				<img
+					src={
+						movieData.Poster !== "N/A"
+							? movieData.Poster
+							: "https://via.placeholder.com/400"
+					}
+					alt={movieData.Title}
+				/>
+			</div>
+
+			<div>
+				<span>{movieData.Type}</span>
+				<h3>{movieData.Title}</h3>
+			</div>
+		</div>
+	);
 };
 
 export default MovieCard;
