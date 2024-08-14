e5c8fdb423b6c3a7c35c39a2ddaf65c2a946cc7a
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-14 02:42:20 -0600

    Adding look of test movie data

--- src/simpleApp/MovieApp.jsx
+++ src/simpleApp/MovieApp.jsx
@@ -4,6 +4,14 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=8744df25";
 
+const testMovie = {
+	Title: "Italian Spiderman",
+	Year: "2007",
+	imdbID: "tt2705436",
+	Type: "movie",
+	Poster: "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+};
+
 const MovieApp = () => {
 	// const [requestCount, setRequestCount] = useState(0);
 
@@ -33,6 +41,30 @@ const MovieApp = () => {
 					onChange={() => {}}
 				/>
 				<img src={SearchIcon} alt="Search" onClick={() => {}} />
+			</div>
+
+			<div className="container">
+				<div className="movie">
+					<div>
+						<p>{testMovie.Title}</p>
+					</div>
+
+					<div>
+						<img
+							src={
+								testMovie.Poster !== "N/A"
+									? testMovie.Poster
+									: "https://via.placeholder.com/400"
+							}
+							alt={testMovie.Title}
+						/>
+					</div>
+
+					<div>
+						<span>{testMovie.Type}</span>
+						<h3>{testMovie.Title}</h3>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
