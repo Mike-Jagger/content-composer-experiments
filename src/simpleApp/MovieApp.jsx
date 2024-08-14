89ecbb1d42c882295bd7b23ae1cccb62dcdd0e67
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-14 02:19:14 -0600

    adding method to start search on component render

--- src/simpleApp/MovieApp.jsx
+++ src/simpleApp/MovieApp.jsx
@@ -1,14 +1,32 @@
+import { useState, useEffect } from "react";
 import "./MovieApp.css";
 import SearchIcon from "./search.svg";
 
-const fetchMovies = async (movieName) => {
-	const response = await fetch({
-		url: "http://www.omdbapi.com/?i=tt3896198&apikey=8744df25",
-	});
-};
+const API_URL = "http://www.omdbapi.com/?apikey=8744df25";
 
 const MovieApp = () => {
-	return <h1>Movie App</h1>;
+	// const [requestCount, setRequestCount] = useState(0);
+
+	const searchMovies = async (title) => {
+		// setRequestCount(requestCount + 1);
+
+		const response = await fetch(`${API_URL}&s=${title}`);
+
+		const data = await response.json();
+
+		console.log(data.Search);
+	};
+
+	useEffect(() => {
+		// searchMovies("spiderman");
+	}, []);
+
+	return (
+		<div className="app">
+			<h1>Movie App</h1>
+			{/* <p style={{ color: "white" }}>{requestCount}</p> */}
+		</div>
+	);
 };
 
 export default MovieApp;
