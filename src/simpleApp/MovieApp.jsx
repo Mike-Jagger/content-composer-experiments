1ac75440c7a88f1e5d18ab93f74c21dcb7367af5
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-14 02:47:02 -0600

    Adding movie card component

--- src/simpleApp/MovieApp.jsx
+++ src/simpleApp/MovieApp.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 import "./MovieApp.css";
 import SearchIcon from "./search.svg";
 
