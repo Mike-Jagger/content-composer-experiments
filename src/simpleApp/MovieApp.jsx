6c8b56fb4d4692f97f0e2089095521feb6b91487
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-21 13:58:05 -0600

    removing unused hook

--- src/simpleApp/MovieApp.jsx
+++ src/simpleApp/MovieApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MovieCard from "./MovieCard";
 import "./MovieApp.css";
 import SearchIcon from "./search.svg";
