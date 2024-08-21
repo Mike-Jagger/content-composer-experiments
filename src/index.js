845e05732676e8eed42874a9e752d74b24599cec
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-21 15:01:25 -0600

    Adding method to change experiment based on envionment variable

--- src/index.js
+++ src/index.js
@@ -3,17 +3,35 @@ import ReactDOM from "react-dom/client";
 import MovieApp from "./simpleApp/MovieApp";
 // import BudgetApp from "./reactHooks/budgetApp/BudgetApp";
 // import Hooks from "./reactHooks/hooks/Hooks";
+// import DashboardApp from "./reactHooks/tailwind/DashboardApp";
 // import HightlightEffects from "./highlighting/HighlightEffects";
-// import Experiment from "./composerEngine/Experiment";
+// import ComposerEngine from "./composerEngine/Experiment";
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const experimentToRun = process.env;
+const experimentToRun = process.env.REACT_APP_EXPERIMENT_SELECTION;
+console.log(process.env);
+
+function experimentToRender(experiment) {
+	switch (experiment) {
+		case "movieApp":
+			return <MovieApp />;
+		case "dashboardApp":
+			break;
+		case "reactHooks":
+			break;
+		case "budgetApp":
+			break;
+		case "highlights":
+			break;
+		case "composerEngine":
+			break;
+		default:
+			return <p>No experiment found/selected</p>;
+	}
+}
 
 root.render(
-	<React.StrictMode>
-		{}
-		<MovieApp />
-	</React.StrictMode>
+	<React.StrictMode>{experimentToRender(experimentToRun)}</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
