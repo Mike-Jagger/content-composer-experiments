3c73a3fa054c4c31ba491bc1e810a5010162cb1e
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-21 13:36:40 -0600

    commenting out test movie

--- src/simpleApp/MovieApp.jsx
+++ src/simpleApp/MovieApp.jsx
@@ -5,13 +5,13 @@ import SearchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=8744df25";
 
-const testMovie = {
-	Title: "Italian Spiderman",
-	Year: "2007",
-	imdbID: "tt2705436",
-	Type: "movie",
-	Poster: "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-};
+// const testMovie = {
+// 	Title: "Italian Spiderman",
+// 	Year: "2007",
+// 	imdbID: "tt2705436",
+// 	Type: "movie",
+// 	Poster: "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+// };
 
 const MovieApp = () => {
 	// const [requestCount, setRequestCount] = useState(0);
