7ee791e2ab4f59579313f4eaa6ff92bee90b38ed
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-22 01:09:39 -0600

    nesting inner routes within Layout component

--- src/dashboardApp/components/shared/Layout.jsx
+++ src/dashboardApp/components/shared/Layout.jsx
@@ -5,15 +5,16 @@ const Layout = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<h1>This is the main page</h1>} />
-				<Route
-					path="/invoices"
-					element={<h1>This is the page to display invoices</h1>}
-				/>
-				<Route
-					path="/dashboard"
-					element={<h1>Welcome to your dashboard</h1>}
-				/>
+				<Route path="/" element={<h1>This is the main page</h1>}>
+					<Route
+						path="/invoices"
+						element={<h1>This is the page to display invoices</h1>}
+					/>
+					<Route
+						path="/dashboard"
+						element={<h1>Welcome to your dashboard</h1>}
+					/>
+				</Route>
 			</Routes>
 		</Router>
 	);
