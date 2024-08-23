19a80d8af5a918b79b87903d56a87a8cb54e5f5b
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-23 14:59:11 -0600

    Adding method to display pages that have and do not have header

--- src/dashboardApp/components/shared/Layout.jsx
+++ src/dashboardApp/components/shared/Layout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Searchbar from "./Searchbar";
 
 const Layout = () => {
+	const { pathname } = useLocation();
+	const pathsWithHeader = ["/"];
+
 	return (
 		<div className="h-screen w-screen overflow-hidden bg-neutral-100 flex flex-row">
 			<Sidebar />
 			<div className="flex flex-col flex-1">
-				<Searchbar />
+				{pathsWithHeader.includes(pathname) ? <Searchbar /> : null}
 				<div>{<Outlet />}</div>
 			</div>
 		</div>
