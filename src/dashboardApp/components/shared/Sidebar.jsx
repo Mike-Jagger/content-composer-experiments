803fa1faacb0b888a36bf375f518764b123b1e5a
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-23 13:35:39 -0600

    Adding sidebar and logo styles

--- src/dashboardApp/components/shared/Sidebar.jsx
+++ src/dashboardApp/components/shared/Sidebar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { FcBullish } from "react-icons/fc";
 
 const Sidebar = () => {
 	return (
-		<div className="">
-			<div>logo</div>
+		<div className="flex flex-col bg-neutral-900 w-60 p-3">
+			<div className="flex flex-row items-center gap-2 px-1 py-3">
+				<FcBullish fontSize={24} />
+				<span className="text-neutral-200 text-lg">OpenShop</span>
+			</div>
 			<div>routes</div>
 			<div>bottom links</div>
 		</div>
