26145291b70c85d85e56176039e2d16b458ff718
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-23 14:21:41 -0600

    Adding sidebar route links

--- src/dashboardApp/components/shared/Sidebar.jsx
+++ src/dashboardApp/components/shared/Sidebar.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 import { FcBullish } from "react-icons/fc";
+import { Link, useLocation } from "react-router-dom";
+import {
+	DASHBOARD_SIDEBAR_ROUTE_LINKS,
+	DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+} from "../../lib/constants";
+
+let linkClass =
+	"flex flex-row items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
+
+function SidebarLink({ link }) {
+	const { pathname } = useLocation();
+	console.log(linkClass);
+	return (
+		<Link
+			className={`${
+				pathname === link.path
+					? "bg-neutral-700 text-white"
+					: "text-neutral-400"
+			} + ${linkClass}`}
+			to={link.path}
+		>
+			{link.icon}
+			<span>{link.label}</span>
+		</Link>
+	);
+}
 
 const Sidebar = () => {
 	return (
@@ -8,7 +34,11 @@ const Sidebar = () => {
 				<FcBullish fontSize={24} />
 				<span className="text-neutral-200 text-lg">OpenShop</span>
 			</div>
-			<div>routes</div>
+			<div className="flex flex-col flex-1 gap-0.5">
+				{DASHBOARD_SIDEBAR_ROUTE_LINKS.map((link) => {
+					return <SidebarLink key={link.key} link={link} />;
+				})}
+			</div>
 			<div>bottom links</div>
 		</div>
 	);
